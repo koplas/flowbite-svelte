38ba34e25f9668546ffb581c01fa941ea008ea49
koplas <54645365+koplas@users.noreply.github.com>
2024-09-25 12:20:31 +0200

    Add new eslint flat config file

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import parser from 'svelte-eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  {
+    ignores: ['**/*.cjs', '.vercel/', '.svelte-kit/']
+  },
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:svelte/recommended'),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint
+    },
+
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+
+      // TODO: Enable this rule
+      '@typescript-eslint/no-explicit-any': 'off'
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module'
+    }
+  },
+  {
+    files: ['**/*.svelte'],
+
+    languageOptions: {
+      parser: parser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  }
+];
